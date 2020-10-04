34eb2f227e577534d098814ebed529344af15430
DavidDuefrene <fortismilite@gmail.com>
2020-10-04 01:24:57 -0600

    Added documentation and also added JSDoc with better-docs

--- src/InfiniteLoad/InfiniteLoad.jsx
+++ src/InfiniteLoad/InfiniteLoad.jsx
@@ -1,3 +1,7 @@
+/**
+ * @module InfiniteLoad
+ * @exports InfiniteLoad
+ */
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
@@ -5,24 +9,58 @@ import CSS from './InfiniteLoad.module.css';
 
 /**
  * Displays a infinite scroll object
- * @extends Component
- * @param {func} loadMore The function to load more items
- * @param {list} objects The list of objects that is being displayed
- * @param {bool} useButtons If a button should be displayed
- * @param {element} customButton The button to be displayed
- * @param {bool} horizontal If the component should be displayed horizontally instead of vertically
+ * @component
+ * @requires react
+ * @requires prop-types
+ * @author David Duefrene
  */
 const InfiniteLoad = (props) => {
     const {
         objects, loadMore, useButtons, customButton, horizontal,
     } = props;
+    /**
+     * If the list should be displayed horizontally or vertically
+     * @const
+     * @type {CSS}
+     * @default [Vertical]
+     */
     const listStyle = horizontal ? CSS.Horizontal : CSS.Vertical;
+    /**
+     * The Style of the button
+     * @const
+     * @type {CSS}
+     */
     const buttonStyle = horizontal ? CSS.ButtonHor : CSS.ButtonVert;
+    /**
+     * The button itself
+     * @type {HTMLElement}
+     */
     let button = customButton !== null ? customButton : null;
 
+    /**
+     * Handles the vertical scroll
+     * @const
+     * @type {function}
+     */
     const handleScroll = () => {
+        /**
+         * Last item in the list
+         * @const
+         * @type {HTMLElement}
+         * TODO Make lastLi in upper scope
+         */
         const lastLi = document.querySelector(`div > ul.${listStyle}`);
+        /**
+         * The location of the last item in the list
+         * @const
+         * @type {float}
+         */
         const lastLiOffset = lastLi.offsetTop + lastLi.clientHeight;
+        /**
+         * The location of the current item/s in view in the list
+         * @const
+         * @type {float}
+         */
         const pageOffset = window.pageYOffset + window.innerHeight;
 
         if (pageOffset > lastLiOffset) {
@@ -30,11 +68,43 @@ const InfiniteLoad = (props) => {
         }
     };
 
+    /**
+     * Handles the horizontal scroll
+     * @const
+     * @type {function}
+     */
     const scrollHorizontally = () => {
+        /**
+         * The Window event
+         * @const
+         * @type {event}
+         */
         const eve = window.event;
+        /**
+         * How much the user scrolled
+         * @const
+         * @type {float}
+         */
+        // TODO do I need to check for all 3 possibilities?
         const delta = eve.deltaY || eve.detail || -eve.wheelDelta;
+        /**
+         * Last item in the list
+         * @const
+         * @type {HTMLElement}
+         * TODO Make lastLi in upper scope
+         */
         const lastLi = document.querySelector(`div > ul.${listStyle}`);
+        /**
+         * The location of the last item in the list
+         * @const
+         * @type {float}
+         */
         const lastLiOffset = lastLi.offsetLeft - lastLi.clientWidth;
+        /**
+         * The location of the current item/s in view in the list
+         * @const
+         * @type {float}
+         */
         const pageOffset = window.pageXOffset - window.innerWidth;
 
         if (pageOffset > lastLiOffset) {
@@ -53,6 +123,7 @@ const InfiniteLoad = (props) => {
         window.addEventListener('wheel', (e) => handleScroll(e));
     });
 
+    // If component is using buttons and a custom button has not been provided use HTML button
     if (useButtons && button === null) {
         button = (
             <button className={buttonStyle} type='button' onClick={loadMore}>
@@ -75,10 +146,25 @@ const InfiniteLoad = (props) => {
 };
 
 InfiniteLoad.propTypes = {
+    /**
+    * loadMore The function to load more items
+    */
     loadMore: PropTypes.func.isRequired,
+    /**
+    * objects The list of objects that is being displayed
+    */
     objects: PropTypes.arrayOf(PropTypes.element).isRequired,
+    /**
+    * useButtons If a button should be displayed
+    */
     useButtons: PropTypes.bool,
+    /**
+    * customButton The button to be displayed
+    */
     customButton: PropTypes.element,
+    /**
+    * horizontal If the component should be displayed horizontally instead of vertically
+    */
     horizontal: PropTypes.bool,
 };
 
